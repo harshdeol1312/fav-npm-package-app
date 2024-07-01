88ce8d60d4b90ca7e1ea56547c914ee408e715ba
maurya <maurya@ensuredit.com>
2024-07-02 02:00:01 +0530

    completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const NavBar = () => (
-  <nav className="mb-4">
-    <ul className="flex">
-      <li className="mr-4">
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/favorites">My Favorites</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="mb-4">
+      <ul className="flex">
+        <li className={`mr-4 ${location.pathname === '/' ? 'text-blue-500' : ''}`}>
+          <Link to="/" activeClassName="text-blue-500">
+            Home
+          </Link>
+        </li>
+        <li className={`${location.pathname === '/favorites' ? 'text-blue-500' : ''}`}>
+          <Link to="/favorites" activeClassName="text-blue-500">
+            My Favorites
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
